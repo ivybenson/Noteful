561713964d745f5cec7360c6791abb21df01b04f
Ivy Benson <irosebenson@gmail.com>
2020-09-29 18:40:19 -0500

    more

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import { Route, Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Note from "./Note/Note";
+import FoldersFile from "./FoldersFile/FoldersFile";
+import SideBar from "./SideBar/SideBar";
+import dummyStore from "./dummy-store";
 
-function App() {
-  return (
-    <div className="App">
-      <header>Noteful</header>
-      <div className="note-list">
-        <Note />
+class App extends Component {
+  state = {
+    notes: [],
+    folders: [],
+  };
+
+  componentDidMount() {
+    setTimeout(() => this.setState(dummyStore), 600);
+  }
+
+  renderMain() {
+    const { notes, folders } = this.state;
+   return ()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <nav></nav>
+        <header>
+          <h1>
+            <Link to="/">Noteful</Link>
+            <FontAwesomeIcon icon="check-double" />
+          </h1>
+        </header>
+        <main className="App__main">{this.renderMain()}</main>
       </div>
-      <div className="folder-list"></div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/Note/Note.js
+++ src/Note/Note.js
@@ -1,6 +1,12 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Note() {
-  return <div>Notes</div>;
+  return (
+    <div>
+      Notes
+      <FontAwesomeIcon icon="plus" />
+    </div>
+  );
 }
