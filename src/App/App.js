0579525c7f0f7b8818cc2fbf044b6485b6ad74fc
Ivy Benson <irosebenson@gmail.com>
2020-10-13 19:56:39 -0500

    added noteError section

--- src/App/App.js
+++ src/App/App.js
@@ -9,6 +9,7 @@ import ApiContext from "../APIContext";
 import config from "../config";
 import AddNote from "../AddNote/AddNote";
 import AddFolder from "../AddFolder/AddFolder";
+import NoteError from "../NoteError";
 import "./App.css";
 
 class App extends Component {
@@ -137,14 +138,16 @@ class App extends Component {
     return (
       <ApiContext.Provider value={value}>
         <div className="App">
-          <nav className="App__nav">{this.renderNavRoutes()}</nav>
-          <header className="App__header">
-            <h1>
-              <Link to="/">Noteful</Link>
-              <FontAwesomeIcon icon="check-double" />
-            </h1>
-          </header>
-          <main className="App__main">{this.renderMainRoutes()}</main>
+          <NoteError>
+            <nav className="App__nav">{this.renderNavRoutes()}</nav>
+            <header className="App__header">
+              <h1>
+                <Link to="/">Noteful</Link>
+                <FontAwesomeIcon icon="check-double" />
+              </h1>
+            </header>
+            <main className="App__main">{this.renderMainRoutes()}</main>
+          </NoteError>
         </div>
       </ApiContext.Provider>
     );
